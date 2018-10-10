17a819dac4a0d403aec176b5edb11cf823a44e8e
Patrick <yangsy9@mail2.sysu.edu.cn>
2018-10-10 16:31:16 +0800

    Update src/bilinear_interpolation.py

--- src/bilinear_interpolation.py
+++ src/bilinear_interpolation.py
@@ -5,9 +5,6 @@ from matplotlib import pyplot as plt
 
 
 def arr_resize(arr, new_size, axis=0):
-    if arr.dtype != np.float64:
-        arr = arr.astype(np.float64)
-
     shape = list(arr.shape)
     old_size = shape[axis]
     shape[axis] = new_size
@@ -40,14 +37,25 @@ def bilinear_resize(im, new_shape):
     if im.dtype != np.float64:
         im = im.astype(np.float64)
 
+    if im.ndim == 2:
+        tmp = arr_resize(im, new_shape[0], axis=0)
+        res = arr_resize(tmp, new_shape[1], axis=1)
+        return res
+    elif im.ndim == 3:
+        res = np.zeros(new_shape)
+        for n, ch in enumerate(im):
+            print(n, ch.shape)
+            # tmp = arr_resize(ch, new_shape[0], axis=0)
+            # res[:, :, n] = arr_resize(tmp, new_shape[1], axis=1)
+        return res
+    else:
+        raise ValueError(f"Image ndim can only be 2 or 3, yet input ndim is {im.ndim}")
 
 
 if __name__ == '__main__':
-    im = plt.imread('images/HMS_Implacable.jpg')
+    im = plt.imread('images/cat.jpg')
     start = time.time()
-    for _ in range(100):
-        im_resized = arr_resize(im, 400, axis=0)
-        im_resized = arr_resize(im_resized, 600, axis=1)
+    im_resized = bilinear_resize(im, [400, 600, 3])
     print(time.time() - start)
 
     plt.subplot(1, 2, 1)
