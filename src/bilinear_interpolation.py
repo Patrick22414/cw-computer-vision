5c18eceb8859873d76ca5702456e45535fdd616e
Patrick <yangsy9@mail2.sysu.edu.cn>
2018-10-11 17:37:21 +0800

    Resizing for coloured images done

--- src/bilinear_interpolation.py
+++ src/bilinear_interpolation.py
@@ -15,47 +15,43 @@ def arr_resize(arr, new_size, axis=0):
         low = int(pos)
         if axis == 0:
             try:
-                vec_l = arr[low, :]
-                vec_u = arr[low + 1, :]
-                res[i, :] = (pos - low) * (vec_u - vec_l) + vec_l
+                vec_l = arr[low, :, :]
+                vec_u = arr[low + 1, :, :]
+                res[i, :, :] = (pos - low) * (vec_u - vec_l) + vec_l
             except IndexError:
-                res[i, :] = arr[low, :]
+                res[i, :] = arr[low, :, :]
         elif axis == 1:
             try:
-                vec_l = arr[:, low]
-                vec_u = arr[:, low + 1]
-                res[:, i] = (pos - low) * (vec_u - vec_l) + vec_l
+                vec_l = arr[:, low, :]
+                vec_u = arr[:, low + 1, :]
+                res[:, i, :] = (pos - low) * (vec_u - vec_l) + vec_l
             except IndexError:
-                res[:, i] = arr[:, low]
+                res[:, i, :] = arr[:, low, :]
         else:
-            raise ValueError
+            raise ValueError("The axis argument can be either 0 or 1")
 
     return res
 
 
 def bilinear_resize(im, new_shape):
     if im.dtype != np.float64:
-        im = im.astype(np.float64)
+        raise TypeError("Image must be of type numpy.float64")
+    if im.ndim != len(new_shape):
+        raise Warning("The new shape and the image are of different ndim")
 
     if im.ndim == 2:
-        tmp = arr_resize(im, new_shape[0], axis=0)
-        res = arr_resize(tmp, new_shape[1], axis=1)
-        return res
-    elif im.ndim == 3:
-        res = np.zeros(new_shape)
-        for n, ch in enumerate(im):
-            print(n, ch.shape)
-            # tmp = arr_resize(ch, new_shape[0], axis=0)
-            # res[:, :, n] = arr_resize(tmp, new_shape[1], axis=1)
-        return res
-    else:
-        raise ValueError(f"Image ndim can only be 2 or 3, yet input ndim is {im.ndim}")
+        im = im[:, :, np.newaxis]
+
+    tmp = arr_resize(im, new_shape[0], axis=0)
+    res = arr_resize(tmp, new_shape[1], axis=1)
+
+    return np.squeeze(res)
 
 
 if __name__ == '__main__':
-    im = plt.imread('images/cat.jpg')
+    im = plt.imread('images/HMS_Implacable.jpg') / 256
     start = time.time()
-    im_resized = bilinear_resize(im, [400, 600, 3])
+    im_resized = bilinear_resize(im, [400, 600])
     print(time.time() - start)
 
     plt.subplot(1, 2, 1)
